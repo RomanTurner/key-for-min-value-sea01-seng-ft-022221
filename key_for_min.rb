baa180f0875ce58a341bc32bead32b281dc0e77a
Roman Turner <rn.turner@live.com>
2021-01-20 22:38:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+ if name_hash.empty?
+   return nil
+ else
   lowest_key = nil
   lowest_value = name_hash[0][0]
   name_hash.each do |k, v|
@@ -10,5 +13,6 @@ def key_for_min_value(name_hash)
       lowest_key = k
     end
   end
+end
   lowest_key
 end
