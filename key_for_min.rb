18a28bd87089fecd09e34332dd6509fbdb98ad59
Roman Turner <rn.turner@live.com>
2021-01-20 22:38:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
  if name_hash.empty?
    return nil
  else
-  lowest_key = nil
+  lowest_key = :k
   lowest_value = name_hash[0][0]
   name_hash.each do |k, v|
     if v < lowest_value
